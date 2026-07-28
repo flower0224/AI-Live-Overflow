16effab25f81c18fa9c8faccefd4cc974062e71f
flower0224 <493725220@qq.com>
2026-07-28 09:16:34 +0800

    Add activity-ktx dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,5 +33,6 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
