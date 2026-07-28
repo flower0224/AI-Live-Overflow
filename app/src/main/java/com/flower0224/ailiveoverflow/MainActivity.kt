42e6810fa3cce1569de2e10df4bde25027826a50
flower0224 <493725220@qq.com>
2026-07-28 12:15:58 +0800

    feat: add usage stats permission check in MainActivity

--- app/src/main/java/com/flower0224/ailiveoverflow/MainActivity.kt
+++ app/src/main/java/com/flower0224/ailiveoverflow/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.flower0224.ailiveoverflow
 
+import android.app.AppOpsManager
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
@@ -22,8 +23,26 @@ class MainActivity : ComponentActivity() {
                 Uri.parse("package:$packageName")
             )
             overlayPermissionLauncher.launch(intent)
+        } else if (!hasUsageStatsPermission()) {
+            Toast.makeText(this, "请开启「使用情况访问」权限，否则无法感知前台App", Toast.LENGTH_LONG).show()
+            val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
+            usageStatsLauncher.launch(intent)
         } else {
             startOverlayService()
+        }
+    }
+
+    private fun hasUsageStatsPermission(): Boolean {
+        return try {
+            val appOps = getSystemService(APP_OPS_SERVICE) as AppOpsManager
+            val mode = appOps.checkOpNoThrow(
+                AppOpsManager.OPSTR_GET_USAGE_STATS,
+                android.os.Process.myUid(),
+                packageName
+            )
+            mode == AppOpsManager.MODE_ALLOWED
+        } catch (_: Exception) {
+            false
         }
     }
 
@@ -32,9 +51,26 @@ class MainActivity : ComponentActivity() {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
                 Settings.canDrawOverlays(this)
             ) {
-                startOverlayService()
+                if (!hasUsageStatsPermission()) {
+                    Toast.makeText(this, "请开启「使用情况访问」权限", Toast.LENGTH_LONG).show()
+                    val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
+                    usageStatsLauncher.launch(intent)
+                } else {
+                    startOverlayService()
+                }
             } else {
                 Toast.makeText(this, "需要悬浮窗权限才能显示宠物哦", Toast.LENGTH_LONG).show()
+            }
+            finish()
+        }
+
+    private val usageStatsLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (hasUsageStatsPermission()) {
+                startOverlayService()
+            } else {
+                Toast.makeText(this, "未开启使用情况访问，宠物将无法感知前台App", Toast.LENGTH_LONG).show()
+                startOverlayService()
             }
             finish()
         }
